6f3549b6cf98c84423f5cf1039d58c4daaa2f0a7
Kuzmin Yevgeniy <eakuzmin87@mail.ru>
2017-04-25 00:02:23 +0300

    ver. 1.0.2 - changes in interface, - add repeat value control features.

--- cows-and-bulls game.py
+++ cows-and-bulls game.py
@@ -15,6 +15,7 @@ class Model:
         self.sec_number_list = []
         self.win = False
         self.round = 1
+        self.wrong_answers = []
 
     def generate_digit(self):
         self.sec_number = randint(0, 10**self.digit_num - 1)
@@ -23,7 +24,6 @@ class Model:
             self.sec_number_list.append(
                 self.sec_number % (10 ** d) // 10 ** (d - 1)
             )
-        print(self.sec_number)
 
     def estimate_value(self, user_number):
         self.bull = 0
@@ -66,61 +66,105 @@ class Model:
         self.round = 1
 
 
+class Controller:
+    def __init__(self, model):
+        self.model = model
+
+    def set_view(self, view):
+        self.view = view
+
+    def start_game(self):
+        digit_num = self.view.get_game_settings()
+        self.model.start_game(digit_num)
+        self.view.start_game()
+
+    def input_digit(self, event=None):
+        digit = self.view.get_digit()
+        if digit:
+            self.model.wrong_answers.append(int(digit))
+            if not self.model.is_win(digit):
+                self.model.estimate_value(digit)
+                self.view.log_wrong_answ()
+                self.model.round += 1
+            else:
+                self.view.game_over()
+
+
 class TkView(Frame):
     font1 = ('Consolas', 14)
     font2 = ('Consolas', 16)
-    font3 = ('Consolas', 18)
+    font3 = ('Consolas', 18, 'bold')
 
     def __init__(self, controller=None, model=None, master=None, **config):
         super().__init__(master)
-        self.pack(expand=YES, fill=BOTH)
-        if model is not None:
-            self.model = model
-        if controller is not None:
-            self.controller = controller
-            controller.set_view(self)
+        self.pack(expand=True, fill='both')
+        self.model = model
+        self.controller = controller
+        self.controller.set_view(self)
         self.create_widgets()
 
     def create_widgets(self):
         # top frame with probably var
         frame_top = Frame(self)
         self.digit_var = StringVar(frame_top)
-        self.digit_var.trace('w',
-                             lambda name, index, mode,
-                                    sv=self.digit_var.trace:
+        self.digit_var.trace('w', lambda name, index, mode, sv=self.digit_var.trace:
                              self.input_control(sv))
         self.answer_ent = Entry(frame_top,
                                 textvariable=self.digit_var,
-                                font=__class__.font2)
+                                state='disabled',
+                                font= __class__.font3)
         self.answer_ent.bind('<Return>', self.controller.input_digit)
-        self.answer_but = Button(frame_top, text='OK', font=__class__.font2)
-        self.answer_but['command'] = self.controller.input_digit
-        self.answer_but['state'] = 'disabled'
-        self.answer_ent['state'] = 'disabled'
+        self.answer_but = Button(frame_top,
+                                 text='OK',
+                                 command=self.controller.input_digit,
+                                 bg='#bed6be',
+                                 state='disabled',
+                                 font=__class__.font3)
         # log
-        self.log_txt = Text(self, width=20, height=10, font=__class__.font1)
-        self.log_txt['state'] = 'disabled'
+        frame_log_parent = Frame(self)
+        frame_log_child = Frame(frame_log_parent)
+        self.log_txt = Text(frame_log_child,
+                            wrap='word',
+                            width=20,
+                            height=10,
+                            state='disabled',
+                            font=__class__.font1)
+        scrollbar = Scrollbar(frame_log_child,
+                              command=self.log_txt.yview)
+        self.log_txt.config(yscrollcommand=scrollbar.set)
         self.log_line = 1
         # bottom frame with settings
-        frame_bottom = Frame(self)
-        label = Label(frame_bottom, text='Number of digits:', font=__class__.font2)
-        self.digit_count_var = StringVar(frame_bottom)
+        frame_bottom_parent = Frame(self, bg='grey')
+        frame_bottom_child = Frame(frame_bottom_parent, bg='grey')
+        start_but = Button(frame_bottom_child,
+                           text='START',
+                           command=self.controller.start_game,
+                           bg='#ffddaa',
+                           font=__class__.font3)
+        label = Label(frame_bottom_child,
+                      text='Number of digits:',
+                      bg='grey',
+                      font=__class__.font2)
+        self.digit_count_var = StringVar()
         self.digit_count_var.set(4)
-        spinbox = Spinbox(frame_bottom,
-                          font=__class__.font2, width=5,
+        spinbox = Spinbox(frame_bottom_child,
+                          textvariable=self.digit_count_var,
                           from_=DIGIT_COUNT_MIN, to=DIGIT_COUNT_MAX,
-                          textvariable=self.digit_count_var)
-        start_but = Button(self, text='START', font=__class__.font2)
-        start_but['command'] = self.controller.start_game
+                          width=3,
+                          font=__class__.font3)
         # packed
-        frame_top.pack(side='top', expand='yes', fill='x', pady=5, padx=10)
+        frame_top.pack(side='top', fill='x', pady=5, padx=5)
         self.answer_ent.pack(side='left', expand='yes', fill='both')
         self.answer_but.pack(side='right')
-        self.log_txt.pack(side='top', expand='yes', fill='x')
-        frame_bottom.pack(side='bottom', expand='yes', fill='x', padx=10, pady=10)
-        label.pack(side='left', expand='yes', fill='x')
-        spinbox.pack(side='left')
-        start_but.pack(side='bottom', fill='x', padx=10, pady=10)
+        frame_log_parent.pack(fill='both', expand=True)
+        frame_log_child.pack(fill='both', expand=True, padx=5)
+        self.log_txt.pack(side='left', expand='yes', fill='both')
+        scrollbar.pack(side='right', fill='y')
+        frame_bottom_parent.pack(side='bottom', fill='x')
+        frame_bottom_child.pack(fill='x', padx=5, pady=5)
+        start_but.pack(side='top', fill='x')
+        label.pack(side='left', expand='yes', fill='x', pady=5)
+        spinbox.pack(side='left', pady=5)
 
     def get_game_settings(self):
         return int(self.digit_count_var.get())
@@ -136,21 +180,25 @@ class TkView(Frame):
         digit = self.digit_var.get()
         if digit:
             if len(digit) == self.model.digit_num:
-                self.answer_ent.delete(0, 'end')
-                return int(digit)
+                if int(digit) in self.model.wrong_answers:
+                    self.answer_ent.config(bg='blue')
+                    self.answer_ent.after(500, lambda: self.answer_ent.config(bg='white'))
+                else:
+                    self.answer_ent.delete(0, 'end')
+                    return int(digit)
             else:
                 self.answer_ent.config(bg='red')
                 self.answer_ent.after(500, lambda: self.answer_ent.config(bg='white'))
 
     def add_to_log(self, string):
-        self.log_txt['state'] = 'normal'
+        self.log_txt.config(state='normal')
         self.log_txt.insert('{}.{}'.format(self.log_line, 0), string)
-        self.log_txt['state'] = 'disabled'
+        self.log_txt.config(state='disabled')
         self.log_txt.see('{0}.{1}'.format(self.log_line, 0))
         self.log_line += 1
 
     def log_wrong_answ(self):
-        template = '\n{}. {}:   cows-{} & bull-{}'
+        template = '\n{}. {}:   cows-{} & bulls-{}'
         message = template.format(str(self.model.round).rjust(2),
                                   self.model.user_number,
                                   self.model.cow,
@@ -158,51 +206,29 @@ class TkView(Frame):
         self.add_to_log(message)
 
     def game_over(self):
-        self.answer_but['state'] = 'disabled'
-        self.answer_ent['state'] = 'disabled'
+        self.answer_but.config(state='disabled')
+        self.answer_ent.config(state='disabled')
         message = '\nCorrect! You find the secret number {} with {} try.'. \
             format(self.model.sec_number, self.model.round)
         self.add_to_log(message)
 
-    def game_start(self):
+    def start_game(self):
         self.log_line = 1
-        self.log_txt['state'] = 'normal'
+        self.log_txt.config(state='normal')
         self.log_txt.delete('1.0', 'end')
-        self.log_txt['state'] = 'disabled'
-        self.answer_but['state'] = 'normal'
-        self.answer_ent['state'] = 'normal'
+        self.log_txt.config(state='disabled')
+        self.answer_but.config(state='normal')
+        self.answer_ent.config(state='normal')
         message = 'Conceive {}-valued secret number, try to guess!'. \
             format(self.model.digit_num)
         self.add_to_log(message)
-
-
-class Controller:
-    def __init__(self, model):
-        self.model = model
-
-    def set_view(self, view):
-        self.view = view
-
-    def start_game(self):
-        digit_num = self.view.get_game_settings()
-        self.model.start_game(digit_num)
-        self.view.game_start()
-
-    def input_digit(self, event=None):
-        digit = self.view.get_digit()
-        if digit:
-            if not self.model.is_win(digit):
-                self.model.estimate_value(digit)
-                self.view.log_wrong_answ()
-                self.model.round += 1
-            else:
-                self.view.game_over()
 
 
 game_model = Model()
 game_controller = Controller(game_model)
 root = Tk()
 root.title('Cows & Bulls')
+root.minsize(width=330, height=385)
 game_view = TkView(master=root,
                    controller=game_controller,
                    model=game_model)
